3802de2e31932b7de23035da35492e273f61b8ba
Jojo <a@b.com>
2020-09-04 15:11:27 +0200

    initial commit

---
+++ src/importme.js
@@ -0,0 +1,7 @@
+import css from './importme.module.scss';
+
+const Importme = () => {
+    return <div className={css.test}>Fuck offzzzyyy!!!</div>
+}
+
+export default Importme;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Importme from './importme';
+
+ReactDOM.render(
+   <Importme />,
+    document.getElementById('root')
+);
